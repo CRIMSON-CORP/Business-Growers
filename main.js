7bf900b7c92496a047879fc14bdfe7517760b748
CRIMSON <olueatoworosanwo@gmail.com>
2021-10-29 08:27:14 +0100

    adjustments, exported partners as img

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const stars = document.querySelectorAll(".star_icon");
 const works = document.querySelectorAll(".work_icon");
-const emails = document.querySelectorAll(".email_icon");
+const email = document.querySelector(".email_icon");
+const ebay = document.querySelector(".ebay");
+const linkedin = document.querySelector(".linkedin");
+const godaddy = document.querySelector(".godaddy");
+const indeed = document.querySelector(".indeed");
 const fetch_star = fetch("/img/star.svg").then((res) => res.text());
 const fetch_work = fetch("/img/work.svg").then((res) => res.text());
 const fetch_email = fetch("/img/email.svg").then((res) => res.text());
@@ -10,6 +14,7 @@ stars.forEach(async (star) => {
 works.forEach(async (work) => {
     work.innerHTML = await fetch_work;
 });
-emails.forEach(async (email) => {
+
+(async function () {
     email.innerHTML = await fetch_email;
-});
+})();
