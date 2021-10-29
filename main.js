0c3272d0c45e40baf8b039ad749d1e2db9a7a272
CRIMSON <olueatoworosanwo@gmail.com>
2021-10-29 07:38:34 +0100

    Done with footer

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const stars = document.querySelectorAll(".star_icon");
 const works = document.querySelectorAll(".work_icon");
+const emails = document.querySelectorAll(".email_icon");
 const fetch_star = fetch("/img/star.svg").then((res) => res.text());
 const fetch_work = fetch("/img/work.svg").then((res) => res.text());
+const fetch_email = fetch("/img/email.svg").then((res) => res.text());
 stars.forEach(async (star) => {
     star.innerHTML = await fetch_star;
 });
 works.forEach(async (work) => {
     work.innerHTML = await fetch_work;
+});
+emails.forEach(async (email) => {
+    email.innerHTML = await fetch_email;
 });
